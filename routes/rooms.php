e81e0b105aaf510234ddcd6d8b6d832356e628b3
VICTOR EDUARDO DIAS DUTRA <victor.eddutra@senacsp.edu.br>
2025-09-10 17:29:11 -0300

    Adiciondno metodo

--- routes/rooms.php
+++ routes/rooms.php
@@ -1,35 +1,30 @@
 <?php
-
 require_once __DIR__ . "/../controllers/QuartoController.php";
-
-if ($_SERVER['REQUEST_METHOD'] === "GET" ){
-    $id = $segmentos[2] ?? null;
-    //QuartoController::buscarPorId($conn, $id);
-    //echo $id;
-    //QuartoController::listarTodos($conn);
-if(isset($id)){
-    QuartoController::buscarPorId($conn, $id);
-}else{
-    QuartoController::listarTodos($conn);
+ 
+if ($_SERVER['REQUEST_METHOD'] === "GET"){
+    $id = $seguimentos[2] ?? null;  
+    if(isset($id)){
+        quartoController::buscarPorId($conn, $id);
+    }else{
+        quartoController::listarTodos($conn);
     }
 }
-
-elseif($_SERVER['REQUEST_METHOD'] === "DELETE" ){
-    $id = $segmentos[2] ?? null;
-
-if(!isset($id)){
-    QuartoController::deletarQuarto($conn, $id);
-}else{
-    jsonResponse(['message' => 'Id do quarto não encontrado'], 400);
-    }
+elseif ($_SERVER['REQUEST_METHOD'] === "DELETE"){
+    $id = $seguimentos[2] ?? null;
+   
+    if(!isset($id)){
+        QuartoController::deletarQuarto($conn, $id);
+    }else{
+        jsonResponse(['messagem' =>'É necessario passar o ID'], 400);
+    }  
 }
-
-
 else{
     jsonResponse([
         'status'=>'erro',
-        'message'=>'Método não permitido'
+        'menssagem'=>'Metodo nao permitido'
     ], 405);
 }
-
+ 
+ 
 ?>
+ 
