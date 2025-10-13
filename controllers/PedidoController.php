286cc0ad6820904007a21518d481b42dbfeaeb76
dutraxz <victoreduardo0896@gmail.com>
2025-10-13 11:38:58 -0300

    feat(Pedidos): adicionando validação da ordem dos pedidos

--- controllers/PedidoController.php
+++ controllers/PedidoController.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . "/../models/PedidoModel.php";
+require_once  "validadorController.php";
 
 class PedidoController{
     public static function criar($conn, $data){
@@ -12,6 +13,26 @@ class PedidoController{
         }
 
     }
+
+    public static ordemPedido($conn, $data){
+        $data["id_usuario_fk"] = isset($data['id_usuario_fk'] ? $data['id_usuario_fk']:: null);
+
+        ValidadorController::validate_data($data, ["id_cliente_fk", "pagamento", "quartos"]);
+        if (!is_array($data['quartos']) || count($data['quartos']) === 0){
+            return jsonResponse(['message'=>"nao existe reservas"], 400);
+        }
+        foreach($data['quartos'] as $index => $quarto){
+            ValidatorController::validate_data($quarto,["id", "dataInicio", "dataFim"]);
+            // Normaliza horas padrão
+            $quarto["dataInicio"] = ValidatorController::dataHora($quarto["dataInicio"], 14);
+            $quarto["dataFim"] = ValidatorController::dataHora($quarto["dataFim"], 12);
+            $data['quartos'][$index] = $quarto;
+        }
+
+        $resultado = PedidoModel::criarOrdem($conn, $data);
+        return jsonResponse($resultado);
+    }
+
 
     public static function listarTodos($conn){
         $listaPedidos = PedidoModel::listarTodos($conn);
